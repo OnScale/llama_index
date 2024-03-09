a0fbfd878954a737e36f650beb87e57c7bcbb7e1
Ryan Peach <rgpeach10@gmail.com>
2024-03-08 21:27:24 -0500

    Check that there are no changes upon test

--- llama-index-packs/llama-index-packs-code-hierarchy/scripts/test_generate_tags.py
+++ llama-index-packs/llama-index-packs-code-hierarchy/scripts/test_generate_tags.py
@@ -1,5 +1,11 @@
 from .generate_tags import main
+import git
 
 
 def test_generate_tags():
     assert main() == 0
+
+
+def test_no_changes():
+    repo = git.Repo(search_parent_directories=True)
+    assert not repo.is_dirty(untracked_files=False)
